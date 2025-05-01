8363fb2579ae9e5f7836616adb3a000f476f98cf
abdiwakmitiku <abdiwakmitiku2nd@gmail.com>
2025-05-01 12:04:56 +0300

    footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
+import "./Footer.css";
+import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import YouTubeIcon from "@mui/icons-material/YouTube";
 
 function Footer() {
   return (
-    <h1>Footer</h1>
-  )
+    <>
+      <div className="footer-outer-container">
+        <div className="footer-inner-container">
+          <div className="footer-icons">
+            <a><FacebookOutlinedIcon /></a>
+            <a><InstagramIcon /></a>
+            <a><YouTubeIcon /></a>
+          </div>
+          <div className="footer-data">
+            <div>
+              <ul>
+                <li>Audio Description</li>
+                <li>Investor Relations</li>
+                <li>Legal Notice</li>
+              </ul>
+            </div>
+            <div>
+              <ul>
+                <li>Help Center</li>
+                <li>Jobs</li>
+                <li>Cookie Preferences</li>
+              </ul>
+            </div>
+            <div>
+              <ul>
+                <li>Gift Cards</li>
+                <li>Terms Of Use</li>
+                <li>Corporate Information</li>
+              </ul>
+            </div>
+            <div>
+              <ul>
+                <li>Media Center</li>
+                <li>Privacy</li>
+                <li>Contact Us</li>
+              </ul>
+            </div>
+          </div>
+          <div className="service-code">
+            <p>Service Code</p>
+          </div>
+          <div className="copy-write">&copy; 1997-2024 Netflix, Inc.</div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Footer
+export default Footer;
--- client/src/components/SharedLayout.jsx
+++ client/src/components/SharedLayout.jsx
@@ -8,7 +8,7 @@ function SharedLayout() {
     <>
       <Header />
       {/* <Outlet /> */}
-      {/* <Footer /> */}
+      <Footer />
     </>
   )
 }
