5f67c4324d5abaf08b7e1d1cc7e2a5af08c8b11e
abdiwakmitiku <abdiwakmitiku2nd@gmail.com>
2025-05-02 18:26:21 +0300

    Banner

--- client/src/components/Page/Banner/Banner.jsx
+++ client/src/components/Page/Banner/Banner.jsx
@@ -22,7 +22,7 @@ function Banner() {
   }, []);
 
   function truncate(str, n) {
-    return str?.lenth > n ? str.substr(0, n - 1) + "..." : str;
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
   }
   return (
     <>
